0dabfac27c4b486bb6f70324ef6f021a7d9c1f2a
Lou Markovski <lou.markovski@gmail.com>
2020-04-25 10:23:13 +1000

    data_pipelines_dags/dags/template.py * standardise the Department name

--- data_pipelines_dags/dags/template.py
+++ data_pipelines_dags/dags/template.py
@@ -8,7 +8,7 @@ import common.task
 
 DESCRIPTION = """Simple book-end DAG template to get you started"""
 
-BASE = common.Base(dag_name='template-DAG', department='coles', description=DESCRIPTION)
+BASE = common.Base(dag_name='template-DAG', department='dept', description=DESCRIPTION)
 
 DAG = airflow.DAG(BASE.dag_id,
                   default_args=BASE.default_args,
